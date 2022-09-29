d865b48a1f1d29226cc64271666cd8a8194e9dbf
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-09-29 13:48:14 -0400

    Added functionality to list.ejs and created new views files

--- app.js
+++ app.js
@@ -30,10 +30,17 @@ app.get("/", function(req,res){
 })
 
 app.post("/", function(req,res) {
-    item = req.body.newItem;
-    items.push(item)
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item)
+        res.redirect("/");
+
+    }
     //console.log(expression)
-    res.redirect("/");
 })
 
 app.get("/work", function(req,res){
@@ -41,7 +48,13 @@ app.get("/work", function(req,res){
 })
 
 app.post("/work", function(req,res){
-    
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
+
+app.get("/about", function(req,res){
+    res.render("about")
 })
 
 app.listen(4000,function(){
